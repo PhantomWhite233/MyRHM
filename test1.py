d5e74a1088e8062ae958b52fc06fac8e50d44f3e
PhantomWhite233 <295872734@qq.com>
2024-07-15 15:11:32 +0800

    07151511

--- test1.py
+++ test1.py
@@ -12,6 +12,10 @@ class MyTopo(Topo):
     def __init__(self):
         super(MyTopo, self).__init__()
 
+        self.hosts = []
+        self.switches = []
+        self.links = []
+
         Host1 = self.addHost("h1", ip="10.0.0.1", mac="00:00:00:00:00:01")
         Host2 = self.addHost("h2", ip="10.0.0.2", mac="00:00:00:00:00:02")
         Host3 = self.addHost("h3", ip="10.0.0.3", mac="00:00:00:00:00:03")
