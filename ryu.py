bd6ab56c15443a424ad233ce7990481aeddc9993
PhantomWhite233 <295872734@qq.com>
2024-07-16 18:11:45 +0800

    07161600_6_1

--- ryu.py
+++ ryu.py
@@ -36,7 +36,7 @@ class MyController(app_manager.RyuApp):
             real_ip = self.real_ips[i]
             virtual_ip = self.virtual_ips[i]
             self.real2virtual[real_ip] = virtual_ip
-            self.real2virtual[virtual_ip] = real_ip
+            self.virtual2real[virtual_ip] = real_ip
 
         print("real to virtual:")
         print(self.real2virtual)
