6d3b9ee2ae6064382d2a9488ab7f1f345b850c68
PhantomWhite233 <295872734@qq.com>
2024-07-19 00:28:05 +0800

    07190028

--- ryu.py
+++ ryu.py
@@ -115,6 +115,7 @@ class MyController(app_manager.RyuApp):
                 break
             except FileNotFoundError:
                 print("real2virtual.json is Occupied")
+        print(real2virtual)
         for key, value in real2virtual.items():
             virtual2real[value] = key
         return real2virtual, virtual2real
