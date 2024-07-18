81aa1a73b6ffbaf02e1fc1646585306b935724be
Phantom233 <295872734@qq.com>
2024-07-18 16:53:47 +0800

    07181653

--- const.py
+++ const.py
@@ -1,3 +1,3 @@
-BLOCK_SIZE = 4  # 虚拟地址块大小
+BLOCK_SIZE = 4  # 真实ip个数
 REAL_IPS = ['10.0.0.%s' % i for i in range(BLOCK_SIZE)]  # 真实地址池
 VIRTUAL_IPS = ['10.0.0.%s' % i for i in range(BLOCK_SIZE, 3 * BLOCK_SIZE)]  # 虚拟地址池
--- startController.py
+++ startController.py
@@ -1,10 +1,10 @@
 import subprocess
 
 # 启动第一个Python文件
-p1 = subprocess.Popen(["python", "testThread_write.py"])
+p1 = subprocess.Popen(["ryu-manager", "ryu.py"])
 
 # 启动第二个Python文件
-p2 = subprocess.Popen(["python", "testThread_read.py"])
+p2 = subprocess.Popen(["python", "startController.py"])
 
 # 等待两个进程完成
 p1.wait()
